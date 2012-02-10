65aed2b6915885c3c01ac911e503a9b203bd7ab2
Matt Benjamin <matt@linuxbox.com>
2012-02-10 15:30:01 -0500

    Finish integrating svc_rqst_lookup_chan.

--- src/svc_rqst.c
+++ src/svc_rqst.c
@@ -133,65 +133,60 @@ int svc_rqst_new_evchan(uint32_t *chan_id /* OUT */, void *u_data,
     struct svc_rqst_rec *sr_rec;
     int code = 0;
 
-    rwlock_wrlock(&svc_rqst_set_.lock);
     sr_rec = mem_alloc(sizeof(struct svc_rqst_rec));
     if (!sr_rec) {
         __warnx("%s: failed allocating svc_rqst_rec", __func__);
-        goto unlock;
+        goto out;
     }
 
     n_id = ++(svc_rqst_set_.next_id);
     sr_rec->id_k = n_id;
     sr_rec->states = SVC_RQST_STATE_NONE;
-    sr_rec->gen = 0;
     sr_rec->u_data = u_data;
+    sr_rec->gen = 0;
     mutex_init(&sr_rec->mtx, NULL);
     opr_rbtree_init(&sr_rec->xprt_q, rqst_xprt_cmpf /* may be NULL */);
 
+    rwlock_wrlock(&svc_rqst_set_.lock);
     if (opr_rbtree_insert(&svc_rqst_set_.t, &sr_rec->node_k)) {
         /* cant happen */
         __warnx("%s: inserted a counted value twice (counter fail)", __func__);
         mem_free(sr_rec, sizeof(struct svc_rqst_rec));
         n_id = 0; /* invalid value */
-    }
-
-    *chan_id = n_id;
-    
-unlock:
+    }    
     rwlock_unlock(&svc_rqst_set_.lock);
 
+    *chan_id = n_id;
+
+out:
     return (code);
 }
 
 int svc_rqst_free_evchan(uint32_t chan_id, uint32_t flags)
 {
+    struct svc_rqst_rec *sr_rec;
     struct opr_rbtree_node *n;
-    struct svc_rqst_rec *sr_rec, trec;
     SVCXPRT *xprt = NULL;
     int code = 0;
 
-    trec.id_k = chan_id;
-
-    rwlock_wrlock(&svc_rqst_set_.lock);
-
-    /* XXX to simplify life for consumers, allow them to unregister
-     * all the xprt associations here */
-    n = opr_rbtree_lookup(&svc_rqst_set_.t, &trec.node_k);
-    if (n) {
-        sr_rec = opr_containerof(n, struct svc_rqst_rec, node_k);
-        mutex_lock(&sr_rec->mtx);
-        /* traverse sr_req->xprt_q inorder */
-        n = opr_rbtree_first(&sr_rec->xprt_q);
-        while (n != NULL) {
-            /* indirect on xp_ev */
-            xprt = opr_containerof(n, struct svc_xprt_ev, node_k)->xprt;
-            /* XXXX TODO: unregister */
-
-            n = opr_rbtree_next(n);
-        }
-        mutex_unlock(&sr_rec->mtx);
+    sr_rec = svc_rqst_lookup_chan(chan_id, SVC_RQST_FLAG_WLOCK);
+    if (! sr_rec) {
+        code = ENOENT;
+        goto unlock;
     }
 
+    /* traverse sr_req->xprt_q inorder */
+    mutex_lock(&sr_rec->mtx);
+    n = opr_rbtree_first(&sr_rec->xprt_q);
+    while (n != NULL) {
+        /* indirect on xp_ev */
+        xprt = opr_containerof(n, struct svc_xprt_ev, node_k)->xprt;
+        /* XXXX TODO: unregister */
+        n = opr_rbtree_next(n);
+    }
+    mutex_unlock(&sr_rec->mtx);
+
+unlock:
     rwlock_unlock(&svc_rqst_set_.lock);
 
     return (code);
@@ -199,28 +194,27 @@ int svc_rqst_free_evchan(uint32_t chan_id, uint32_t flags)
 
 int svc_rqst_evchan_reg(uint32_t chan_id, SVCXPRT *xprt, uint32_t flags)
 {
-    struct svc_rqst_rec *sr_rec, trec;
-    struct opr_rbtree_node *n;
+    struct svc_rqst_rec *sr_rec;
     struct svc_xprt_ev *xp_ev;
     int code = EINVAL;
-    
-    trec.id_k = chan_id;
 
-    rwlock_rdlock(&svc_rqst_set_.lock);
-    n = opr_rbtree_lookup(&svc_rqst_set_.t, &trec.node_k);
-    if (n) {
-        sr_rec = opr_containerof(n, struct svc_rqst_rec, node_k);
-        mutex_lock(&sr_rec->mtx);
-        assert(xprt->xp_ev); /* cf. svc_rqst_init_xprt */
-        xp_ev = (struct svc_xprt_ev *) xprt->xp_ev;
-        if (opr_rbtree_insert(&sr_rec->xprt_q, &xp_ev->node_k)) {
-            /* shouldn't happen */
-            __warnx("%s: SVCXPRT %p already registered",
-                    __func__, xprt);
-        }
-        mutex_unlock(&sr_rec->mtx);
+    sr_rec = svc_rqst_lookup_chan(chan_id, SVC_RQST_FLAG_RLOCK);
+    if (! sr_rec) {
+        code = ENOENT;
+        goto unlock;
     }
 
+    mutex_lock(&sr_rec->mtx);
+    assert(xprt->xp_ev); /* cf. svc_rqst_init_xprt */
+    xp_ev = (struct svc_xprt_ev *) xprt->xp_ev;
+    if (opr_rbtree_insert(&sr_rec->xprt_q, &xp_ev->node_k)) {
+        /* shouldn't happen */
+        __warnx("%s: SVCXPRT %p already registered",
+                __func__, xprt);
+    }
+    mutex_unlock(&sr_rec->mtx);
+
+unlock:
     rwlock_unlock(&svc_rqst_set_.lock);
 
     return (code);
@@ -228,29 +222,29 @@ int svc_rqst_evchan_reg(uint32_t chan_id, SVCXPRT *xprt, uint32_t flags)
 
 int svc_rqst_evchan_unreg(uint32_t chan_id, SVCXPRT *xprt, uint32_t flags)
 {
-    struct svc_rqst_rec *sr_rec, trec;
-    struct opr_rbtree_node *ns, *nx;
+    struct svc_rqst_rec *sr_rec;
+    struct opr_rbtree_node *nx;
     struct svc_xprt_ev *xp_ev;
     int code = EINVAL;
-    
-    trec.id_k = chan_id;
 
-    rwlock_rdlock(&svc_rqst_set_.lock);
-    ns = opr_rbtree_lookup(&svc_rqst_set_.t, &trec.node_k);
-    if (ns) {
-        sr_rec = opr_containerof(ns, struct svc_rqst_rec, node_k);
-        mutex_lock(&sr_rec->mtx);
-        assert(xprt->xp_ev); /* cf. svc_rqst_init_xprt */
-        xp_ev = (struct svc_xprt_ev *) xprt->xp_ev;
-        nx = opr_rbtree_lookup(&sr_rec->xprt_q, &xp_ev->node_k);
-        if (nx)
-            opr_rbtree_remove(&sr_rec->xprt_q, &xp_ev->node_k);
-        else
-            __warnx("%s: SVCXPRT %p found but cant be removed",
-                    __func__, xprt);
-        mutex_unlock(&sr_rec->mtx);
+    sr_rec = svc_rqst_lookup_chan(chan_id, SVC_RQST_FLAG_RLOCK);
+    if (! sr_rec) {
+        code = ENOENT;
+        goto unlock;
     }
 
+    mutex_lock(&sr_rec->mtx);
+    assert(xprt->xp_ev); /* cf. svc_rqst_init_xprt */
+    xp_ev = (struct svc_xprt_ev *) xprt->xp_ev;
+    nx = opr_rbtree_lookup(&sr_rec->xprt_q, &xp_ev->node_k);
+    if (nx)
+        opr_rbtree_remove(&sr_rec->xprt_q, &xp_ev->node_k);
+    else
+        __warnx("%s: SVCXPRT %p found but cant be removed",
+                __func__, xprt);
+    mutex_unlock(&sr_rec->mtx);
+
+unlock:
     rwlock_unlock(&svc_rqst_set_.lock);
 
     return (code);
@@ -272,7 +266,7 @@ int svc_rqst_thrd_run(uint32_t chan_id, uint32_t flags)
      * with a secondary state machine to detect inconsistencies (e.g., trying
      * to unregister a channel when it is active) */
     mutex_lock(&sr_rec->mtx);
-    rwlock_unlock(&svc_rqst_set_.lock);
+    rwlock_unlock(&svc_rqst_set_.lock); /* !RLOCK */
     sr_rec->states |= SVC_RQST_STATE_ACTIVE;
     mutex_unlock(&sr_rec->mtx);
     
