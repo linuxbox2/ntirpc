306912a61886834529ec042a4cd2c203d88893bd
Matt Benjamin <matt@linuxbox.com>
2012-07-04 11:50:57 -0400

    Fix egregious rbtree_x cache bug (taking the wrong modulus).
    Also, add trace logic at LOCK trace level in rbtree_x.h in the
    cacheing code only.
    
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- tirpc/misc/rbtree_x.h
+++ tirpc/misc/rbtree_x.h
@@ -2,6 +2,9 @@
 #ifndef _RBTREE_X_H
 #define _RBTREE_X_H
 
+#include <inttypes.h>
+#include <stdio.h>
+
 #include <misc/rbtree.h>
 
 #define CACHE_LINE_SIZE 64
@@ -20,6 +23,7 @@ struct rbtree_x_part
 struct rbtree_x
 {
     uint32_t npart;
+    int32_t cachesz;
     struct rbtree_x_part *tree;
 };
 
@@ -35,19 +39,29 @@ static inline struct opr_rbtree_node *
 rbtree_x_cached_lookup(struct rbtree_x *xt, struct rbtree_x_part *t,
                        struct opr_rbtree_node *nk, uint64_t hk)
 {
-    struct opr_rbtree_node *nv, *nv_cached;
+    struct opr_rbtree_node *nv_cached, *nv = NULL;
+    uint32_t offset;
 
     if (! t)
         t = rbtx_partition_of_scalar(xt, hk);
-    nv_cached = t->cache[(hk % xt->npart)];
-    if (nv_cached) {
-        if (t->t.cmpf(nv_cached, nk) == 0)
-            nv = nv_cached;
-        else
-            nv = NULL;
-    } else
-        nv = opr_rbtree_lookup(&t->t, nk);
 
+    offset = hk % xt->cachesz;
+    nv_cached = t->cache[offset];
+    if (nv_cached) {
+        if (t->t.cmpf(nv_cached, nk) == 0) {
+            nv = nv_cached;
+            goto out;
+        }
+    }
+    nv = opr_rbtree_lookup(&t->t, nk);
+
+    __warnx(TIRPC_DEBUG_FLAG_LOCK,
+            "rbtree_x_cached_lookup: t %p nk %p nv %p"
+            "(%s hk %"PRIx64" slot/offset %d)",
+            t, nk, nv, (nv_cached) ? "CACHED" : "",
+            hk, offset);
+
+out:
     return (nv);
 }
 
@@ -62,15 +76,22 @@ rbtree_x_cached_insert_wt(struct rbtree_x *xt, struct rbtree_x_part *t,
     struct opr_rbtree_node *v_cached, *nv = NULL;
     uint32_t offset;
 
-    offset = hk % xt->npart;
     if (! t)
         t = rbtx_partition_of_scalar(xt, hk);
+
+    offset = hk % xt->cachesz;
     v_cached = t->cache[offset];
     if (! v_cached)
         nv = t->cache[offset] = nk;
     else {
         nv = opr_rbtree_insert(&t->t, nk);
     }
+
+    __warnx(TIRPC_DEBUG_FLAG_LOCK,
+            "rbtree_x_cached_insert_wt: t %p inserting %p "
+            "(%s hk %"PRIx64" slot/offset %d)",
+            t, nk, (v_cached) ? "chaining" : "caching",
+            hk, offset);
 
     return (nv);
 }
@@ -82,15 +103,22 @@ rbtree_x_cached_remove_wt(struct rbtree_x *xt, struct rbtree_x_part *t,
     struct opr_rbtree_node *v_cached;
     uint32_t offset;
 
-    offset = hk % xt->npart;
     if (! t)
         t = rbtx_partition_of_scalar(xt, hk);
+
+    offset = hk % xt->cachesz;
     v_cached = t->cache[offset];
     if (v_cached)
         t->cache[offset] = NULL;
     else {
         return (opr_rbtree_remove(&t->t, nk));
     }
+
+    __warnx(TIRPC_DEBUG_FLAG_LOCK,
+            "rbtree_x_cached_remove_wt: t %p removing %p "
+        "(%s hk %"PRIx64" slot/offset %d)",
+        t, nk, (v_cached) ? "caching" : "chaining",
+        hk, offset);
 }
 
 #endif /* _RBTREE_X_H */
