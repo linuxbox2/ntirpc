8cbabedef8e191a373aaed18d52b578506257090
Max Matveev <makc@gmx.co.uk>
2012-11-13 11:20:02 -0500

    Use correct socket option for PKTINFO on UDPv6
    IPV6_PKTINFO from RFC 2292 has been renamed to IPV6_RECVPKTINFO in RFC 3542.
    
    Signed-off-by: Max Matveev <makc@gmx.co.uk>
    Signed-off-by: Steve Dickson <steved@redhat.com>

--- src/svc_dg.c
+++ src/svc_dg.c
@@ -655,7 +655,7 @@ svc_dg_enable_pktinfo(int fd, const struct __rpc_sockinfo *si)
 		break;
 
 	case AF_INET6:
-		(void) setsockopt(fd, SOL_IPV6, IPV6_PKTINFO, &val, sizeof(val));
+		(void) setsockopt(fd, SOL_IPV6, IPV6_RECVPKTINFO, &val, sizeof(val));
 		break;
 	}
 }
