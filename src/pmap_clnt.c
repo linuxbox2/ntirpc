685dbb8b41d2e3c825aca8a31814328ebadce15c
Sachin Bhamare <sbhamare@panasas.com>
2013-08-06 14:06:21 -0700

    In pmap_set(), make sure to use correct version of INADDR_ANY.
    Signed-off-by: Sachin Bhamare <sbhamare@panasas.com>
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/pmap_clnt.c
+++ src/pmap_clnt.c
@@ -66,8 +66,13 @@ pmap_set(u_long program, u_long version, int protocol, int port)
     if (nconf == NULL) {
         return (FALSE);
     }
-    snprintf(buf, sizeof buf, "0.0.0.0.%d.%d",
+    if (strcmp(nconf->nc_protofmly, NC_INET) == 0) {
+       snprintf(buf, sizeof buf, "0.0.0.0.%d.%d",
+               (((u_int32_t)port) >> 8) & 0xff, port & 0xff);
+    } else if (strcmp(nconf->nc_protofmly, NC_INET6) == 0) {
+       snprintf(buf, sizeof buf, "::.%d.%d",
              (((u_int32_t)port) >> 8) & 0xff, port & 0xff);
+    }
     na = uaddr2taddr(nconf, buf);
     if (na == NULL) {
         freenetconfigent(nconf);
