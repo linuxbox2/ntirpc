c55a77516fd0d226eb46f44b89fec966a682180f
Chuck Lever <chuck.lever@oracle.com>
2015-04-22 11:00:35 -0400

    Add server-side rpc_gss_*() APIs
    Introduce new RPCSEC API functions that match the same libtirpc API
    in FreeBSD and Solaris.  This includes rpc_gss_getcred(3t),
    rpc_gss_svc_max_data_length(3t), rpc_gss_set_svc_name(3t),
    rpc_gss_set_callback(3t), and rpc_gss_get_principal_name(3t).
    
    The man pages, written by Doug Rabson, come from FreeBSD, with
    some adjustments by me.
    
    The new code was written from scratch based on FreeBSD's
    implementation, but adapted to invoke the existing legacy U-M APIs
    in our implementation.  We will maintain the legacy APIs until
    consumers are switched to the new ones.  FreeBSD never had the
    legacy U-M API.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Signed-off-by: Steve Dickson <steved@redhat.com>

--- src/svc_auth_gss.c
+++ src/svc_auth_gss.c
@@ -34,13 +34,27 @@
 
  */
 
+#include <sys/types.h>
+
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pwd.h>
+#include <grp.h>
+
 #include <rpc/rpc.h>
-#include <gssapi/gssapi.h>
+#include <rpc/rpcsec_gss.h>
+
+#include <gssapi/gssapi_ext.h>
+
+#define UNUSED(x) UNUSED_ ## x __attribute__((unused))
 
 extern SVCAUTH svc_auth_none;
+
+/* Internal only */
+bool_t rpc_gss_oid_to_mech(rpc_gss_OID, char **);
+bool_t rpc_gss_num_to_qop(char *, u_int, char **);
 
 /*
  * from mit-krb5-1.2.1 mechglue/mglueP.h:
@@ -73,6 +87,10 @@ struct svc_rpc_gss_data {
 	u_int			seqlast;	/* last sequence number */
 	u_int32_t		seqmask;	/* bitmask of seqnums */
 	gss_name_t		client_name;	/* unparsed name string */
+	rpc_gss_rawcred_t	rcred;		/* raw credential */
+	rpc_gss_ucred_t		ucred;		/* cooked credential */
+	gid_t			gids[NGRPS];	/* list of groups */
+	void *			cookie;		/* callback cookie */
 };
 
 #define SVCAUTH_PRIVATE(auth) \
@@ -80,7 +98,8 @@ struct svc_rpc_gss_data {
 
 /* Global server credentials. */
 static gss_cred_id_t	_svcauth_gss_creds;
-static gss_name_t	_svcauth_gss_name = NULL;
+static gss_name_t	_svcauth_gss_name = GSS_C_NO_NAME;
+static char *		_svcauth_svc_name = NULL;
 
 bool_t
 svcauth_gss_set_svc_name(gss_name_t name)
@@ -89,7 +108,7 @@ svcauth_gss_set_svc_name(gss_name_t name)
 
 	gss_log_debug("in svcauth_gss_set_svc_name()");
 
-	if (_svcauth_gss_name != NULL) {
+	if (_svcauth_gss_name != GSS_C_NO_NAME) {
 		maj_stat = gss_release_name(&min_stat, &_svcauth_gss_name);
 
 		if (maj_stat != GSS_S_COMPLETE) {
@@ -97,7 +116,7 @@ svcauth_gss_set_svc_name(gss_name_t name)
 				maj_stat, min_stat);
 			return (FALSE);
 		}
-		_svcauth_gss_name = NULL;
+		_svcauth_gss_name = GSS_C_NO_NAME;
 	}
 	maj_stat = gss_duplicate_name(&min_stat, name, &_svcauth_gss_name);
 
@@ -138,14 +157,14 @@ svcauth_gss_import_name(char *service)
 }
 
 static bool_t
-svcauth_gss_acquire_cred(void)
+svcauth_gss_acquire_cred(u_int req_time, gss_OID_set_desc *oid_set)
 {
 	OM_uint32	maj_stat, min_stat;
 
 	gss_log_debug("in svcauth_gss_acquire_cred()");
 
-	maj_stat = gss_acquire_cred(&min_stat, _svcauth_gss_name, 0,
-				    GSS_C_NULL_OID_SET, GSS_C_ACCEPT,
+	maj_stat = gss_acquire_cred(&min_stat, _svcauth_gss_name, req_time,
+				    oid_set, GSS_C_ACCEPT,
 				    &_svcauth_gss_creds, NULL, NULL);
 
 	if (maj_stat != GSS_S_COMPLETE) {
@@ -174,6 +193,52 @@ svcauth_gss_release_cred(void)
 	_svcauth_gss_creds = NULL;
 
 	return (TRUE);
+}
+
+static rpc_gss_service_t
+_rpc_gss_svc_to_service(rpc_gss_svc_t svc)
+{
+	switch (svc) {
+	case RPCSEC_GSS_SVC_NONE:
+		return rpcsec_gss_svc_none;
+	case RPCSEC_GSS_SVC_INTEGRITY:
+		return rpcsec_gss_svc_integrity;
+	case RPCSEC_GSS_SVC_PRIVACY:
+		return rpcsec_gss_svc_privacy;
+	}
+	return rpcsec_gss_svc_default;
+}
+
+static bool_t
+_rpc_gss_fill_in_creds(struct svc_rpc_gss_data *gd, struct rpc_gss_cred *gc)
+{
+	rpc_gss_rawcred_t *rcred = &gd->rcred;
+	OM_uint32 maj_stat, min_stat;
+	gss_buffer_desc buf;
+
+	rcred->version = gc->gc_v;
+	if (!rpc_gss_oid_to_mech(gd->sec.mech, &rcred->mechanism))
+		return FALSE;
+	rcred->service = _rpc_gss_svc_to_service(gd->sec.svc);
+	maj_stat = gss_export_name(&min_stat, gd->client_name, &buf);
+	if (maj_stat != GSS_S_COMPLETE) {
+		gss_log_status("gss_export_name", maj_stat, min_stat);
+		return FALSE;
+	}
+
+	rcred->client_principal = calloc(1, sizeof(rpc_gss_principal_t) +
+								buf.length);
+	if (rcred->client_principal == NULL) {
+		(void)gss_release_buffer(&min_stat, &buf);
+		return FALSE;
+	}
+	rcred->client_principal->len = buf.length;
+	(void)memcpy(rcred->client_principal->name, buf.value, buf.length);
+	(void)gss_release_buffer(&min_stat, &buf);
+
+	rcred->svc_principal = _svcauth_svc_name;
+
+	return TRUE;
 }
 
 static bool_t
@@ -248,6 +313,9 @@ svcauth_gss_accept_sec_context(struct svc_req *rqst,
 				maj_stat, min_stat);
 			return (FALSE);
 		}
+		if (!_rpc_gss_fill_in_creds(gd, gc))
+			return (FALSE);
+
 #ifdef HAVE_KRB5
 		{
 			gss_buffer_desc mechname;
@@ -460,12 +528,12 @@ _svcauth_gss(struct svc_req *rqst, struct rpc_msg *msg, bool_t *no_dispatch)
 		if (rqst->rq_proc != NULLPROC)
 			return (AUTH_FAILED);		/* XXX ? */
 
-		if (_svcauth_gss_name == NULL) {
+		if (_svcauth_gss_name == GSS_C_NO_NAME) {
 			if (!svcauth_gss_import_name("nfs"))
 				return (AUTH_FAILED);
 		}
 
-		if (!svcauth_gss_acquire_cred())
+		if (!svcauth_gss_acquire_cred(0, GSS_C_NULL_OID_SET))
 			return (AUTH_FAILED);
 
 		if (!svcauth_gss_accept_sec_context(rqst, &gr))
@@ -535,6 +603,8 @@ svcauth_gss_destroy(SVCAUTH *auth)
 
 	if (gd->client_name)
 		gss_release_name(&min_stat, &gd->client_name);
+	if (gd->rcred.client_principal != NULL)
+		free(gd->rcred.client_principal);
 
 	mem_free(gd, sizeof(*gd));
 	mem_free(auth, sizeof(*auth));
@@ -594,4 +664,286 @@ svcauth_gss_get_principal(SVCAUTH *auth)
 	pname[gd->cname.length] = '\0';
 
 	return (pname);
+}
+
+/*
+ * External API: Return maximum data size for a security mechanism and transport
+ *
+ * rqst: an incoming RPC request
+ * maxlen: transport's maximum data size, in bytes
+ *
+ * Returns maximum data size given transformations done by current
+ * security setting of "auth", in bytes, or zero if that value
+ * cannot be determined.
+ */
+int
+rpc_gss_svc_max_data_length(struct svc_req *rqst, int maxlen)
+{
+	OM_uint32 max_input_size, maj_stat, min_stat;
+	struct svc_rpc_gss_data	*gd;
+	int conf_req_flag;
+	int result;
+
+	if (!rqst)
+		return 0;
+
+	gd = SVCAUTH_PRIVATE(rqst->rq_xprt->xp_auth);
+
+	switch (gd->rcred.service) {
+	case rpcsec_gss_svc_none:
+		return maxlen;
+	case rpcsec_gss_svc_default:
+	case rpcsec_gss_svc_integrity:
+		conf_req_flag = 0;
+		break;
+	case rpcsec_gss_svc_privacy:
+		conf_req_flag = 1;
+		break;
+	default:
+		return 0;
+	}
+
+	result = 0;
+	maj_stat = gss_wrap_size_limit(&min_stat, gd->ctx, conf_req_flag,
+					gd->sec.qop, maxlen, &max_input_size);
+	if (maj_stat == GSS_S_COMPLETE)
+		if ((int)max_input_size > 0)
+			result = (int)max_input_size;
+	return result;
+}
+
+/*
+ * External API: Set server's GSS principal
+ *
+ * principal: NUL-terminated C string containing GSS principal
+ * mechanism: NUL-terminated C string containing GSS mechanism name
+ * req_time: time in seconds until credential expires
+ * program: program number of the RPC service
+ * version: version number of the RPC service
+ *
+ * Returns TRUE if successful, otherwise FALSE is returned.
+ */
+bool_t
+rpc_gss_set_svc_name(char *principal, char *mechanism, u_int req_time,
+		u_int UNUSED(program), u_int UNUSED(version))
+{
+	gss_OID_set_desc oid_set;
+	rpc_gss_OID oid;
+	char *save;
+
+	if (principal == NULL)
+		return FALSE;
+	save = strdup(principal);
+	if (save == NULL)
+		return FALSE;
+
+	if (!rpc_gss_mech_to_oid(mechanism, &oid))
+		goto out_err;
+	oid_set.count = 1;
+	oid_set.elements = (gss_OID)oid;
+
+	if (!svcauth_gss_import_name(principal))
+		goto out_err;
+	if (!svcauth_gss_acquire_cred(req_time, &oid_set))
+		goto out_err;
+
+	free(_svcauth_svc_name);
+	_svcauth_svc_name = save;
+	return TRUE;
+
+out_err:
+	free(save);
+	return FALSE;
+}
+
+static void
+_rpc_gss_fill_in_ucreds(struct svc_rpc_gss_data *gd)
+{
+	rpc_gss_ucred_t *ucred = &gd->ucred;
+	OM_uint32 maj_stat, min_stat;
+	struct passwd pwd, *pw;
+	long buflen;
+	uid_t uid;
+	char *buf;
+	int len;
+
+	/* default: "nfsnobody" */
+	ucred->uid = 65534;
+	ucred->gid = 65534;
+	ucred->gidlen = 0;
+	ucred->gidlist = gd->gids;
+
+	maj_stat = gss_pname_to_uid(&min_stat, gd->client_name,
+						gd->sec.mech, &uid);
+	if (maj_stat != GSS_S_COMPLETE)
+		return;
+
+	buflen = sysconf(_SC_GETPW_R_SIZE_MAX);
+	if (buflen == -1)
+		return;
+	buf = malloc((size_t)buflen);
+	if (buf == NULL)
+		return;
+
+	(void)getpwuid_r(uid, &pwd, buf, buflen, &pw);
+	if (pw == NULL) {
+		free(buf);
+		return;
+	}
+
+	ucred->uid = pw->pw_uid;
+	ucred->gid = pw->pw_gid;
+	len = NGRPS;
+	(void)getgrouplist(pw->pw_name, pw->pw_gid, ucred->gidlist, &len);
+	ucred->gidlen = len;
+
+	free(buf);
+}
+
+/*
+ * External API: Retrieve requesting client's GSS credential
+ *
+ * rqst: an incoming RPC request
+ * rcred: address of pointer to fill in, or NULL
+ * ucred: address of pointer to fill in, or NULL
+ * cookie: address of pointer to fill in, or NULL
+ *
+ * Returns TRUE if successful. "rcred," "ucred," and "cookie"
+ * are filled in with pointers to the relevant structures.
+ * Caller must not free the memory returned by this API.
+ */
+bool_t
+rpc_gss_getcred(struct svc_req *rqst, rpc_gss_rawcred_t **rcred,
+		rpc_gss_ucred_t **ucred, void **cookie)
+{
+	struct svc_rpc_gss_data	*gd;
+	SVCAUTH *auth;
+
+	if (rqst == NULL)
+		return FALSE;
+
+	if (rqst->rq_xprt->xp_verf.oa_flavor != RPCSEC_GSS)
+		return FALSE;
+
+	auth = rqst->rq_xprt->xp_auth;
+	gd = SVCAUTH_PRIVATE(auth);
+
+	if (rcred != NULL) {
+		auth->raw_cred = gd->rcred;
+		auth->raw_cred.service = _rpc_gss_svc_to_service(gd->sec.svc);
+		(void)rpc_gss_num_to_qop(auth->raw_cred.mechanism, gd->sec.qop,
+						&auth->raw_cred.qop);
+		*rcred = &auth->raw_cred;
+	}
+
+	if (ucred != NULL) {
+		_rpc_gss_fill_in_ucreds(gd);
+		*ucred = &gd->ucred;
+	}
+
+	if (cookie != NULL)
+		*cookie = gd->cookie;
+
+	return TRUE;
+}
+
+/*
+ * External API: Register a callback function
+ *
+ * callback: callback structure containing function and parameters
+ *
+ * Returns TRUE if successful, otherwise FALSE.
+ *
+ * "callback" is copied by rpc_gss_set_callback(), and may be freed
+ * immediately after it returns.
+ */
+bool_t
+rpc_gss_set_callback(rpc_gss_callback_t *callback)
+{
+	/* not yet supported */
+	return FALSE;
+}
+
+/*
+ * External API: Form a generic rpc_gss_principal
+ *
+ * principal: address of buffer to fill in
+ * mechanism: NUL-terminated C string containing GSS mechanism name
+ * user_name: NUL-terminated C string containing user or service principal
+ * node: NUL-terminated C string containing a hostname
+ * secdomain: NUL-terminated C string containing a security realm
+ *
+ * Returns TRUE if successful, otherwise FALSE.  Caller must free
+ * returned "principal" with free(3).
+ */
+bool_t
+rpc_gss_get_principal_name(rpc_gss_principal_t *principal, char *mechanism,
+		char *user_name, char *node, char *secdomain)
+{
+	OM_uint32 maj_stat, min_stat;
+	rpc_gss_principal_t result;
+	size_t nodelen, secdomlen;
+	gss_name_t name, mechname;
+	gss_buffer_desc namebuf;
+	rpc_gss_OID oid;
+
+
+	if (principal == NULL || user_name == NULL || strlen(user_name) == 0)
+		return FALSE;
+	if (!rpc_gss_mech_to_oid(mechanism, &oid))
+		return FALSE;
+
+	nodelen = 0;
+	if (node != NULL)
+		nodelen = strlen(node) + 1;
+	secdomlen = 0;
+	if (secdomain != NULL)
+		secdomlen = strlen(secdomain) + 1;
+	namebuf.length = strlen(user_name) + nodelen + secdomlen;
+	namebuf.value = calloc(1, namebuf.length);
+	if (namebuf.value == NULL)
+		return FALSE;
+	(void)strcpy(namebuf.value, user_name);
+	if (nodelen > 0) {
+		(void)strcat(namebuf.value, "/");
+		(void)strcat(namebuf.value, node);
+	}
+	if (secdomlen > 0) {
+		(void)strcat(namebuf.value, "@");
+		(void)strcat(namebuf.value, secdomain);
+	}
+
+	maj_stat = gss_import_name(&min_stat, &namebuf,
+					GSS_C_NT_USER_NAME, &name);
+	free(namebuf.value);
+	if (maj_stat != GSS_S_COMPLETE) {
+		gss_log_status("gss_import_name", maj_stat, min_stat);
+		return FALSE;
+	}
+
+	maj_stat = gss_canonicalize_name(&min_stat, name, oid, &mechname);
+	(void)gss_release_name(&min_stat, &name);
+	if (maj_stat != GSS_S_COMPLETE) {
+		gss_log_status("gss_canonicalize_name", maj_stat, min_stat);
+		return FALSE;
+	}
+
+	maj_stat = gss_export_name(&min_stat, mechname, &namebuf);
+	(void)gss_release_name(&min_stat, &mechname);
+	if (maj_stat != GSS_S_COMPLETE) {
+		gss_log_status("gss_export_name", maj_stat, min_stat);
+		return FALSE;
+	}
+
+	result = calloc(1, sizeof(*result) + namebuf.length);
+	if (result == NULL) {
+		(void)gss_release_buffer(&min_stat, &namebuf);
+		return FALSE;
+	}
+	result->len = namebuf.length;
+	(void)memcpy(result->name, namebuf.value, namebuf.length);
+	(void)gss_release_buffer(&min_stat, &namebuf);
+
+	*principal = result;
+	return TRUE;
 }
