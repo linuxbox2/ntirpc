b73ac50c9def8166645146ce1c81400a87b828e6
Marek Szuba <Marek.Szuba@cern.ch>
2016-10-05 10:40:20 +0200

    Fix paths in libntirpc.pc

--- libntirpc.pc.in.cmake
+++ libntirpc.pc.in.cmake
@@ -1,11 +1,11 @@
-prefix=@prefix@
-exec_prefix=@exec_prefix@
-libdir=@libdir@
-includedir=@includedir@
+prefix=@CMAKE_INSTALL_PREFIX@
+exec_prefix=@CMAKE_INSTALL_PREFIX@
+libdir=@CMAKE_INSTALL_PREFIX@/@LIB_INSTALL_DIR@
+includedir=@CMAKE_INSTALL_PREFIX@/include
 
 Name: libntirpc
 Description: New Transport Independent RPC Library
 Requires:
 Version: @NTIRPC_VERSION@
-Libs: -L@libdir@ -lintirpc
-Cflags: -I@includedir@/ntirpc
+Libs: -L${libdir} -lintirpc
+Cflags: -I${includedir}/ntirpc
