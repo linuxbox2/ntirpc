14b26b44cf383291bfd76ee3dd5b356cb2f434a9
Matt Benjamin <mbenjamin@redhat.com>
2017-05-04 14:46:18 -0400

    XDR and netbuf:  fix decoder overruns from guidovranken
    Fixes issues reported in RPCB and generic XDR routines.  In ntirpc,
    xdr_inline.h updated along with xdr.c
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rpc_generic.c
+++ src/rpc_generic.c
@@ -759,6 +759,8 @@ __rpc_taddr2uaddr_af(int af, const struct netbuf *nbuf)
 
 	switch (af) {
 	case AF_INET:
+		if (nbuf->len < sizeof(*sin))
+			return NULL;
 		sin = nbuf->buf;
 		if (inet_ntop(af, &sin->sin_addr, namebuf, sizeof(namebuf))
 		    == NULL)
@@ -773,6 +775,8 @@ __rpc_taddr2uaddr_af(int af, const struct netbuf *nbuf)
 		break;
 #ifdef INET6
 	case AF_INET6:
+		if (nbuf->len < sizeof(*sin6))
+			return NULL;
 		sin6 = nbuf->buf;
 		if (inet_ntop(af, &sin6->sin6_addr, namebuf6, sizeof(namebuf6))
 		    == NULL) {
@@ -839,6 +843,8 @@ __rpc_uaddr2taddr_af(int af, const char *uaddr)
 
 	port = 0;
 	sin = NULL;
+	if (uaddr == NULL)
+		return NULL;
 	addrstr = mem_strdup(uaddr);
 
 	/*
