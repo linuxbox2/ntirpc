9765afe12d9079dbbaef1e4d3d0a8d7b2104c038
Daniel Gryniewicz <dang@redhat.com>
2019-10-21 08:53:11 -0400

    XDR IOQ - Update position to end of XDR
    When calling allochdrs(), it needs to update the posision to the end of
    the XDR, or else only partial data is sent.  The position had been
    previously updated to the location of the length field.
    
    Signed-off-by: Daniel Gryniewicz <dang@redhat.com>
    Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

--- src/xdr_ioq.c
+++ src/xdr_ioq.c
@@ -1106,6 +1106,7 @@ xdr_ioq_allochdrs(XDR *xdrs, u_int start, xdr_vio *vector, int iov_count)
 	int idx = 0;
 	struct xdr_ioq *xioq = XIOQ(xdrs);
 	struct poolq_entry *have;
+	u_int totlen = start;
 
 	/* update the most recent data length, just in case */
 	xdr_tail_update(xdrs);
@@ -1175,6 +1176,9 @@ xdr_ioq_allochdrs(XDR *xdrs, u_int start, xdr_vio *vector, int iov_count)
 
 		uv = xdr_ioq_use_or_allocate(xioq, &vector[idx], uv);
 
+		/* Record used space */
+		totlen += vector[idx].vio_length;
+
 		/* Advance to next (DATA) buffer */
 		idx++;
 	}
@@ -1195,6 +1199,9 @@ xdr_ioq_allochdrs(XDR *xdrs, u_int start, xdr_vio *vector, int iov_count)
 		__warnx(TIRPC_DEBUG_FLAG_XDR,
 			"Skipping idx %d for VIO_DATA",
 			idx);
+
+		/* Record used space */
+		totlen += vector[idx].vio_length;
 
 		if (have != NULL) {
 			/* Next buffer exists */
@@ -1235,9 +1242,15 @@ xdr_ioq_allochdrs(XDR *xdrs, u_int start, xdr_vio *vector, int iov_count)
 
 		uv = xdr_ioq_use_or_allocate(xioq, &vector[idx], uv);
 
+		/* Record used space */
+		totlen += vector[idx].vio_length;
+
 		/* Next vector buffer */
 		idx++;
 	}
+
+	/* Update position to end of the last buffer */
+	XDR_SETPOS(xdrs, start + totlen);
 
 	return true;
 }
